02128e7896a373110ee0afc5c0445e38b529fb4a
Tarun Patidar <tarunpatidar6557@gmail.com>
2022-01-21 12:55:00 +0530

    Session 1

---
+++ Session 1/src/Myapp.java
@@ -0,0 +1,11 @@
+
+public class Myapp {
+
+	public static void main(String[] args) {
+		System.out.println("We are learning java. stick to the concepts well");
+		System.out.println("This is all for the very 1st session");
+		System.out.println("Thank You. I hope you all enjoyed");
+
+	}
+
+}
